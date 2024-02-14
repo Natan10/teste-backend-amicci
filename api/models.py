76d868f37e059b80f090cdbbf831ceed5fceee18
Natan moreira <natan-moreira1@hotmail.com>
2024-02-14 10:23:05 -0300

    feat: create models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+
+# Varejista
+class Retailer(models.Model):
+    name = models.CharField(max_length=165)
+
+# Fornecedor
+class Vendor(models.Model):
+    name = models.CharField(max_length=165)
+    retailer = models.ForeignKey(Retailer, on_delete=models.SET_NULL, null=True)
+
+
+class Briefing(models.Model):
+    name = models.CharField(max_length=165)
+    responsible = models.CharField(max_length=255)
+    release_date = models.DateTimeField(auto_now_add=True)
+    available = models.BooleanField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    retailer = models.ForeignKey(Retailer, on_delete=models.CASCADE)
